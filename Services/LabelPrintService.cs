25393fc5e6f4ce478c38aa4c277e63c70e43b306
Rinot RAKOTOMALALA <130461482+Fiainana@users.noreply.github.com>
2026-09-07 08:07:55 +0300

    feat: optimize printing for thermal label printers

--- Services/LabelPrintService.cs
+++ Services/LabelPrintService.cs
@@ -23,22 +23,34 @@ public static class LabelPrintService
 
     public static FixedDocument BuildDocument(IReadOnlyList<Article> articles, ConnectionConfig config)
     {
+        // Taille exacte de l'étiquette en DIP (96 DPI)
         double pageW = LabelRenderService.MmToPx(config.LabelWidthMm);
         double pageH = LabelRenderService.MmToPx(config.LabelHeightMm);
+
         var document = new FixedDocument();
         document.DocumentPaginator.PageSize = new Size(pageW, pageH);
 
         foreach (var article in articles)
         {
             var label = LabelRenderService.CreateLabel(article, config);
-            var pageContent = new PageContent();
-            var fixedPage = new FixedPage { Width = pageW, Height = pageH, Background = Brushes.White };
+
+            var fixedPage = new FixedPage
+            {
+                Width = pageW,
+                Height = pageH,
+                Background = Brushes.White
+            };
+
+            // Positionnement précis (pas de marge)
             FixedPage.SetLeft(label, 0);
             FixedPage.SetTop(label, 0);
             fixedPage.Children.Add(label);
+
+            var pageContent = new PageContent();
             ((System.Windows.Markup.IAddChild)pageContent).AddChild(fixedPage);
             document.Pages.Add(pageContent);
         }
+
         return document;
     }
 
@@ -50,20 +62,50 @@ public static class LabelPrintService
         if (articles.Count == 0) return false;
 
         var dialog = new PrintDialog();
-        var preferred = PrinterService.FindQueue(config.PreferredPrinterName);
-        if (preferred is not null) dialog.PrintQueue = preferred;
 
+        // Imprimante préférée
+        var preferred = PrinterService.FindQueue(config.PreferredPrinterName);
+        if (preferred is not null)
+            dialog.PrintQueue = preferred;
+
+        // Configuration optimisée pour imprimante thermique
         try
         {
-            dialog.PrintTicket.PageMediaSize = new PageMediaSize(
+            var ticket = dialog.PrintTicket;
+
+            // Taille de page exacte de l'étiquette
+            ticket.PageMediaSize = new PageMediaSize(
                 LabelRenderService.MmToPx(config.LabelWidthMm),
                 LabelRenderService.MmToPx(config.LabelHeightMm));
+
+            // Orientation portrait
+            ticket.PageOrientation = PageOrientation.Portrait;
+
+            // Qualité maximale (important pour les codes-barres)
+            ticket.OutputQuality = OutputQuality.High;
+
+            // Pas de mise à l'échelle
+            ticket.PageScalingFactor = 100;
+
+            // Bordures minimales
+            try
+            {
+                ticket.PageBorderless = PageBorderless.Borderless;
+            }
+            catch { /* certains drivers ne supportent pas */ }
+
+            dialog.PrintTicket = ticket;
         }
-        catch { }
+        catch
+        {
+            // On continue même si le driver ne supporte pas toutes les options
+        }
 
         bool needDialog = showDialog || preferred is null;
-        if (needDialog && dialog.ShowDialog() != true) return false;
+        if (needDialog && dialog.ShowDialog() != true)
+            return false;
 
+        // Impression
         dialog.PrintDocument(BuildDocument(articles, config).DocumentPaginator, description);
         return true;
     }
